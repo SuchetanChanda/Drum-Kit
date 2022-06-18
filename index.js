a09782a6ea9a0254a71cedd33ae9fd303a64f534
SuchetanChanda <chanda.suchetan@gmail.com>
2022-06-18 21:19:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+for(let i = 0 ; i<document.querySelectorAll(".drum").length ; i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        var buttonHTML = this.innerHTML;
+        beat(buttonHTML);
+        buttonAnimation(buttonHTML);
+    });
+    
+}
+document.addEventListener("keydown" , function (event) {
+    beat(event.key);
+    buttonAnimation(event.key);
+})
+
+function beat (s) {
+    switch (s) {
+        case "w":
+            var audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+
+        case "a":
+            var audio = new Audio('./sounds/tom-2.mp3');
+            audio.play();
+            break;
+
+        case "s" :
+            var audio = new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+            
+        case "d" :
+            var audio = new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;  
+
+        case "j":
+            var audio = new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;   
+        
+        case "k" :
+            var audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;    
+
+        case "l":
+            var audio = new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+            break;    
+        default:
+            break;
+    }
+}
+
+function buttonAnimation (curKey)
+{
+    document.querySelector("."+curKey).classList.add("pressed");
+
+    setTimeout(() => {
+        document.querySelector("."+curKey).classList.remove("pressed");
+    }, 100);
+    
+}
+
